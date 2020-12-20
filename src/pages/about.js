f3efd2fb82d8b5b3843b56cfc17f6c8a35db48c5
unknown <pedro.coding.contact@gmail.com>
2020-12-20 15:03:17 -0300

    markdown query working

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,15 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 
-const AboutPage = () => {
+
+const AboutPage = () => {  
   return (
     <Layout>
       <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur sequi,
-        reiciendis molestiae porro veniam repudiandae nulla delectus officia
-        minima laudantium!
-      </p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae nesciunt, exercitationem reiciendis culpa error iste dolore hic vel inventore. Similique.</p>      
       <Link to="/contact">Contact me.</Link>
     </Layout>
   )
