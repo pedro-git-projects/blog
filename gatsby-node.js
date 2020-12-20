40db5352b5ead8a9bfde317a6134898325d40c19
unknown <pedro.coding.contact@gmail.com>
2020-12-20 17:00:40 -0300

    added slugs for blog posts

---
+++ gatsby-node.js
@@ -0,0 +1,17 @@
+const { create } = require('domain')
+const path = require('path')
+
+module.exports.onCreateNode = ({node, actions}) =>{
+ const {createNodeField} = actions
+
+ if(node.internal.type === 'MarkdownRemark'){
+    const slug = path.basename(node.fileAbsolutePath, '.md')
+    
+    createNodeField({
+        node,
+        name: 'slug',
+        value: slug
+    })
+ }
+    
+}
