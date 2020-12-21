1dadc1b44dba9e308484b744d1f3d6e87d635907
unknown <pedro.coding.contact@gmail.com>
2020-12-21 13:19:26 -0300

    contentful posts showing up

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import { graphql } from 'gatsby'
 
-export const query = graphql`
+/* export const query = graphql`
 query (
   $slug: String!
 ) {
@@ -20,16 +20,23 @@ query (
     html
   }
 }
-` 
+` */ 
+
+export const query = graphql`
+query($slug: String!){
+  contentfulBlogPost(slug:{eq: $slug}){
+    title
+    publishDate(formatString:"MMMM Do, YYYY") 
+  }
+}`
 
 const Blog = (props) => {
   return (
   <Layout>
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
-
-  </Layout>)
+      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <p>{props.data.contentfulBlogPost.publishDate}</p>
+  </Layout>
+  )
 }
 
 export default Blog;
